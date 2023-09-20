28e5edf45bf7254647367937a74ca353699d375a
layz <99089467+itzlayz@users.noreply.github.com>
2023-09-20 21:54:31 +0600

    Add files via upload

---
+++ pon.py
@@ -0,0 +1,59 @@
+# 	 @@@@@@    @@@@@@   @@@@@@@  @@@@@@@    @@@@@@   @@@@@@@@@@    @@@@@@   @@@@@@@   @@@  @@@  @@@       @@@@@@@@   @@@@@@
+# 	@@@@@@@@  @@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@       @@@@@@@@  @@@@@@@
+# 	@@!  @@@  !@@         @@!    @@!  @@@  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@@  @@!  @@@  @@!       @@!       !@@
+# 	!@!  @!@  !@!         !@!    !@!  @!@  !@!  @!@  !@! !@! !@!  !@!  @!@  !@!  @!@  !@!  @!@  !@!       !@!       !@!
+# 	@!@!@!@!  !!@@!!      @!!    @!@!!@!   @!@  !@!  @!! !!@ @!@  @!@  !@!  @!@  !@!  @!@  !@!  @!!       @!!!:!    !!@@!!
+# 	!!!@!!!!   !!@!!!     !!!    !!@!@!    !@!  !!!  !@!   ! !@!  !@!  !!!  !@!  !!!  !@!  !!!  !!!       !!!!!:     !!@!!!
+# 	!!:  !!!       !:!    !!:    !!: :!!   !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!!  !!:  !!!  !!:       !!:            !:!
+# 	:!:  !:!      !:!     :!:    :!:  !:!  :!:  !:!  :!:     :!:  :!:  !:!  :!:  !:!  :!:  !:!   :!:      :!:           !:!
+# 	::   :::  :::: ::      ::    ::   :::  ::::: ::  :::     ::   ::::: ::   :::: ::  ::::: ::   :: ::::   :: ::::  :::: ::
+# 	 :   : :  :: : :       :      :   : :   : :  :    :      :     : :  :   :: :  :    : :  :   : :: : :  : :: ::   :: : :
+#
+#                                             © Copyright 2023
+#
+#                                    https://t.me/Den4ikSuperOstryyPer4ik
+#                                                  and
+#                                          https://t.me/ToXicUse
+#
+#                                    🔒 Licensed under the GNU AGPLv3
+#                                 https://www.gnu.org/licenses/agpl-3.0.html
+#
+# meta developer: @AstroModules
+# Rewrited to teagram by itzlayz, pon?
+
+import random
+from telethon.tl.types import Message
+from .. import loader
+
+
+@loader.module('вахуипон', 'itzlayz')
+class ВахуиПонMod(loader.Module):
+    """пон и вахуи"""
+
+    strings = {"name": "ВАХУИ-ПОН", "pon": "пон", "vahui": "вахуи"}
+
+    @loader.command()
+    async def понcmd(self, message: Message):
+        """--> пон"""
+        reply = await message.get_reply_message()
+        m = random.choice(await self.client.get_messages("@PON_STICKS", limit=100))
+        if reply:
+            await self.client.send_message(message.chat_id, file=m, reply_to=reply)
+        else:
+            await message.respond(file=m)
+
+        if message.out:
+            await message.delete()
+
+    @loader.command()
+    async def вахуиcmd(self, message: Message):
+        """--> вахуи"""
+        reply = await message.get_reply_message()
+        m = random.choice(await self.client.get_messages("@VAHUI_STICKS", limit=100))
+        if reply:
+            await self.client.send_message(message.chat_id, file=m, reply_to=reply)
+        else:
+            await message.respond(file=m)
+
+        if message.out:
+            await message.delete()
