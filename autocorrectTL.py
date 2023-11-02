683b3335de5d97ee2254b00dc4993981e9f52961
layz <broitzlayz@gmail.com>
2023-11-02 21:58:37 +0600

    Update autocorrectTL.py

--- autocorrectTL.py
+++ autocorrectTL.py
@@ -14,7 +14,7 @@ from ..types import Config, ConfigValue
 import requests
 
 @loader.module(name="auto-correct-tl", author="itzlayz", version=1.2)
-@loader.warning("Module may interfere with userbot")
+@loader.warning
 class AutoCorrectMod(loader.Module):
     """❤️ Автоматическое исправление текста."""
     def __init__(self):
